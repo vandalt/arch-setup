d8c0b6e9122c76b35d1d52c9c768df340e3d0b42
Thomas Vandal <thomas.vandal@mail.mcgill.ca>
2020-06-01 15:56:07 -0400

    install script

--- install.sh
+++ install.sh
@@ -7,10 +7,12 @@ DISKPART="diskpartlayout"
 HOSTNAME="watson"
 
 # time and date
+echo "Setting time and date..."
 timedatectl set-ntp true
 timedatectl set-timezone $TIMEZONE
 
 # disk partitions
+echo "Partitioning disk"
 if [ ! -f $DISKPART ]; then
     echo "ERROR: Disk partition file not found."
     exit 1
@@ -18,29 +20,36 @@ fi
 sfdisk /dev/"$DEVICE" < diskpartlayout
 
 # filesystem
+echo "Creating file system"
 mkfs.fat -F32 /dev/"$DEVICE"1
 mkfs.ext4 /dev/"$DEVICE"2
 
 # mount
+echo "Mounting partitions..."
 mount /dev/"$DEVICE"2 /mnt
 mkdir -p /mnt/boot/EFI
 mount /dev/"$DEVICE"1 /mnt/boot/EFI
 
 # sort mirrors
+echo "Sorting mirrors..."
 pacman -Sy pacman-contrib --noconfirm
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.back
 curl -s "https://www.archlinux.org/mirrorlist/?country=CA&country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 6 - > /etc/pacman.d/mirrorlist
 
 # main packages with pacstrap
+echo "Pacstrap..."
 pacstrap /mnt base linux linux-firmware
 
 # generate fstab
+echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # chroot to new install
-arch-chroot /mnt
+echo "Entering new install"
+arch-chroot /mnt /bin/bash <<EOF
 
 # swap file
+echo "Creating swap..."
 fallocate -l 4GB /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
@@ -48,16 +57,19 @@ swapon /swapfile
 printf "\n#swap\n/swapfile none swap 0 0\n" >> /etc/fstab
 
 # clock
+echo "Setting clock..."
 ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
 hwclock --systohc
 
 # localization
+echo "Setting localization..."
 sed -i '/en_CA.UTF/s/^#//g' /etc/locale.gen
 locale-gen
 echo "LANG=en_CA.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=cf" >> /etc/vconsole.conf
 
 # Network
+echo "Setting network..."
 echo "$HOSTNAME" >> /etc/hostname
 echo "127.0.0.1    localhost" >> /etc/hosts
 echo "::1          localhost" >> /etc/hosts
