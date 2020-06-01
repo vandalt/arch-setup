576f0126523bdebc1fa00854c268470493a8b0c4
Thomas Vandal <thomas.vandal@mail.mcgill.ca>
2020-06-01 16:29:00 -0400

    edited chroot

---
+++ inchroot.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+# swap file
+echo "Creating swap..."
+fallocate -l 4GB /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+printf "\n#swap\n/swapfile none swap 0 0\n" >> /etc/fstab
+
+# clock
+echo "Setting clock..."
+ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+hwclock --systohc
+
+# localization
+echo "Setting localization..."
+sed -i '/en_CA.UTF/s/^#//g' /etc/locale.gen
+locale-gen
+echo "LANG=en_CA.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=cf" >> /etc/vconsole.conf
+
+# Network
+echo "Setting network..."
+echo "$HOSTNAME" >> /etc/hostname
+echo "127.0.0.1    localhost" >> /etc/hosts
+echo "::1          localhost" >> /etc/hosts
+echo "127.0.1.1    $HOSTNAME.localdomain    $HOSTNAME" >> /etc/hosts
+
+# root password
+echo "Will now run passwd to set root password..."
+passwd
+
+# last installs
+pacman -Syu --noconfirm
+pacman -S grub efibootmgr os-prober networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers intel-ucode --noconfirm
+
+# setup GRUB bootloader
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+sed -i -E '/GRUB_TIMEOUT=/s/[[:digit:]]+/0/g' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
--- install.sh
+++ install.sh
@@ -46,48 +46,10 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # chroot to new install
 echo "Entering new install"
-arch-chroot /mnt /bin/bash <<EOF
-
-# swap file
-echo "Creating swap..."
-fallocate -l 4GB /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-printf "\n#swap\n/swapfile none swap 0 0\n" >> /etc/fstab
-
-# clock
-echo "Setting clock..."
-ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
-hwclock --systohc
-
-# localization
-echo "Setting localization..."
-sed -i '/en_CA.UTF/s/^#//g' /etc/locale.gen
-locale-gen
-echo "LANG=en_CA.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=cf" >> /etc/vconsole.conf
-
-# Network
-echo "Setting network..."
-echo "$HOSTNAME" >> /etc/hostname
-echo "127.0.0.1    localhost" >> /etc/hosts
-echo "::1          localhost" >> /etc/hosts
-echo "127.0.1.1    $HOSTNAME.localdomain    $HOSTNAME" >> /etc/hosts
-
-# root password
-echo "Will now run passwd to set root password..."
-passwd
-
-# last installs
-pacman -Syu --noconfirm
-pacman -S grub efibootmgr os-prober networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers intel-ucode --noconfirm
-
-# setup GRUB bootloader
-grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-sed -i -E '/GRUB_TIMEOUT=/s/[[:digit:]]+/0/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+wget https://raw.githubusercontent.com/vandalt/arch-setup/master/inchroot.sh
+chmod +x inchroot.sh
+mv inchroot.sh /mnt
+arch-chroot /mnt ./inchroot.sh
 
 # sucess message
 echo "Done with config. You can exit, umount -a, and reboot"
